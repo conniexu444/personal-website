3596f1a3b722cc542d33e4b4b3057c56f97bcd47
conniexu444 <conniexu444@gmail.com>
2025-06-07 16:04:19 -0400

    Add in

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/lyrical-libations/", // <-- Use your repo name here
   plugins: [react(), tailwindcss()],
 })
